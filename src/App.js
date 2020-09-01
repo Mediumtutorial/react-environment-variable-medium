bd27fa660799f4b1b7c391eb062c18e8c22a67f8
Manish Mandal <manish.mandal21@gmail.com>
2020-09-01 23:54:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{process.env.REACT_APP_TITLE}</h1>
+    <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
+
+    {process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}
     </div>
   );
 }
